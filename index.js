d55ecae2356635acec6b064d62523940d0529670
hieudevhouse <nguyenminhhieu2003hd@gmail.com>
2026-04-16 14:57:25 +0700

    fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const vectorRoute = require("./routes/vector.route");
 
 const app = express(); // ✅ Khai báo trước
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
